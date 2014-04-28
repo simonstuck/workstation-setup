0600fe4b106a5874afcf46f4ab2e4e7aa80df000
Simon Stueckemann <me@simonstuck.com>
2014-04-27 20:35:53 -0700

    Adding git cookbook for git sensible configuration.

---
+++ cookbooks/git/recipes/default.rb
@@ -0,0 +1,14 @@
+template "#{ENV['HOME']}/.gitignore_global" do
+  mode 0755
+  owner ENV['USER']
+  group Etc.getgrgid(Process.gid).name
+  source "dot.gitignore_global.erb"
+end
+
+template "#{ENV['HOME']}/.gitconfig" do
+  mode 0755
+  owner ENV['USER']
+  group Etc.getgrgid(Process.gid).name
+  source "dot.gitconfig.erb"
+  variables({ :home => ENV['HOME'] })
+end
