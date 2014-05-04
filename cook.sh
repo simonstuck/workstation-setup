c8f3e8f7e1d4ace3668f3dfe6072b56b71ceec4b
Simon Stueckemann <me@simonstuck.com>
2014-05-03 22:52:46 -0700

    Adding script for easy cooking.

--- cook.sh
+++ cook.sh
@@ -14,10 +14,26 @@ wrong_usage () {
   die -e $msg
 }
 
+# Select the role or exit if the role (dish) is not available.
 role="roles/"$1".json"
 
-if [[ "$#" != 1 ]] || [[! -f $role ]]; then
+if [[ "$#" != 1 ]] || [[ ! -f $role ]]; then
   wrong_usage
+fi
+
+# Install chef if it is not installed yet
+if [ ! $(which chef-solo) ]; then
+  echo "Your personal chef is not here yet..."
+  echo "Inviting your personal chef first"
+
+  installed=$(gem install chef 2>/dev/null)
+  if [[ ! $installed ]]; then
+    echo "It looks like I need more rights to install chef."
+
+    if [[ ! $(sudo gem install chef) ]]; then
+      die "Could not install chef."
+    fi
+  fi
 fi
 
 echo "Begin cooking..."
